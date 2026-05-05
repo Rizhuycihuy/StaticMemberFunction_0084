1bdf5fcd396f615a660264c3ee433c9a6161469e
Rizhuycihuy <rizki003131@gmail.com>
2026-05-05 18:07:42 +0700

    definisi dan implementassi

--- conatructionoverloading.cpp
+++ conatructionoverloading.cpp
@@ -13,3 +13,25 @@ class mahasiswa{
           mahasiswa(int iNim, string iNama);
           void cetak();
 };
+
+mahasiswa::mahasiswa(){
+}
+
+mahasiswa::mahasiswa(int iNim){
+  nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama){
+  nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama){
+  nim = iNim;
+  nama = iNama;
+}
+
+void mahasiswa::cetak(){
+  cout << endl << "nim = "<< nim << endl;
+  cout << "nama = " << nama <<endl;
+}
+
